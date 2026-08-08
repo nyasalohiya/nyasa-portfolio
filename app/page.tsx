bc7ef3138ea1f2017b70beed24529a99ec52ca5b
Nyasa Lohiya <189743242+nyasalohiya@users.noreply.github.com>
2026-08-08 09:09:13 +0530

    Uncomment CurrentlyBuilding component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <Hero />
       <About />
-      {/* <CurrentlyBuilding /> */}
+      <CurrentlyBuilding />
       <TechStack />
       <Projects />
       <MoreProjects />
